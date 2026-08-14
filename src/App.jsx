6c3d72f9ac7c164f4a68ceac540cf613718915c5
AleixMJ <aleixtechIT@gmail.com>
2026-08-14 22:28:13 +0200

    feat(ui): added Download PDF button

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Work from './components/Work.jsx'
 import Education from './components/Education.jsx'
 
 function App() {
-  
-
   return (
-    <>
-      <h1>CV Application</h1>
-      <General />
+    <main>
+      <div className="top-bar">
+        <h1>CV Application</h1>
+        <button type="button" onClick={() => window.print()} className="download-btn">
+          📄 Download PDF
+        </button>
+      </div>
+
+      <General/>
       <Work />
       <Education />
-    </>
-  )
+    </main>
+  );
 }
 
 export default App
